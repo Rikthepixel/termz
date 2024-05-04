45178d86800be1970fe5feb859fefac79144b79e
RikThePixel <rikdenbreejen@tutanota.com>
2024-05-04 20:16:28 +0200

    feat: ensure propper vscode version is installed

--- src/drivers/vscode-terminal.ts
+++ src/drivers/vscode-terminal.ts
@@ -7,9 +7,13 @@ import { createConnection } from "net";
 import { readFile } from "fs/promises";
 import { Profile } from "src/models/profile";
 import which from "which";
+import { satisfies as satisfiesVersion } from "semver";
 
 const SOCKET_REGISTRY_FILE = path.join(os.tmpdir(), "termz-vscode-sockets");
 const KNOWN_CLIS = ["code", "code-insiders", "codium", "codium-insiders", "mrcode"] as const;
+const EXTENSION_ID = "rikthepixel.termz";
+const EXTENSION_MIN_INSTALL_VERSION = "1.1.0";
+const SUPPORTED_EXTENSION_VERSION = ">=1.1.0";
 
 async function readRegistry(): Promise<Record<string, string>> {
     return await readFile(SOCKET_REGISTRY_FILE)
@@ -38,7 +42,7 @@ async function getInstalledClis(): Promise<string[]> {
 
 async function getExtensionId(): Promise<string> {
     if (process.env.NODE_ENV === "production") {
-        return "rikthepixel.termz";
+        return EXTENSION_ID;
     } else {
         const pluginVsix = path.resolve(__dirname, "../../", "./plugins/vscode/termz.vsix");
 
@@ -52,11 +56,22 @@ async function getExtensionId(): Promise<string> {
 }
 
 async function syncExtension(cli: string, extensionId: string) {
-    const { stdout } = await $`${cli} --list-extensions`;
-    const foundExtension = stdout.split("\n").find((e) => e.endsWith(".termz"));
-    if (foundExtension) return;
+    const { stdout } = await $`${cli} --list-extensions --show-versions`;
+    const foundExtension = stdout
+        .split("\n")
+        .map((line) => line.split("@"))
+        .find((line) => line[1]!.endsWith(".termz"));
 
-    await $({ stdout: "inherit" })`${cli} --install-extension ${extensionId}`;
+    if (!foundExtension) {
+        await $({ stdout: "inherit" })`${cli} --install-extension ${extensionId}`;
+        return;
+    }
+
+    if (satisfiesVersion(foundExtension[1]!, SUPPORTED_EXTENSION_VERSION)) {
+        return;
+    }
+
+    await $({ stdout: "inherit" })`${cli} --install-extension ${extensionId}@${EXTENSION_MIN_INSTALL_VERSION}`;
 }
 
 async function sendProfileToPipe(profile: Profile, ipcPath: string) {
