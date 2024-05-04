d7bac9d584ad31804fc0b47ca3430e267a7c7b5b
RikThePixel <rikdenbreejen@tutanota.com>
2024-05-04 21:23:41 +0200

    fix: set npm registry

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Command } from "commander";
 import { runCommand } from "./commands/run";
 // import { createCommand } from "./commands/create";
 
-const program = new Command("termz").description("Setup terminal sessions with ease").version("0.3.2");
+const program = new Command("termz").description("Setup terminal sessions with ease").version("0.3.3");
 
 program.addCommand(runCommand, { isDefault: true });
 //.addCommand(createCommand);
