ade2cd34886a4a649a8e809a9a80614694566547
RikThePixel <rikdenbreejen@tutanota.com>
2024-05-06 23:09:39 +0200

    chore: bump version

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Command } from "commander";
 import { runCommand } from "./commands/run";
 // import { createCommand } from "./commands/create";
 
-const program = new Command("termz").description("Setup terminal sessions with ease").version("0.3.3");
+const program = new Command("termz").description("Setup terminal sessions with ease").version("0.4.0", "-v, --version");
 
 program.addCommand(runCommand, { isDefault: true });
 //.addCommand(createCommand);
